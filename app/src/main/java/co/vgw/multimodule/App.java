bbbbee2b1fdbb0dee0e831ba251a4a1ee67dcd7c
Lee Campbell <lee.campbell@vgw.co>
2019-11-30 14:38:35 +0800

    Multiple modules with failing test

---
+++ app/src/main/java/co/vgw/multimodule/App.java
@@ -0,0 +1,10 @@
+package co.vgw.multimodule;
+
+import co.vgw.multimodule.domain.MyModel;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println(new MyModel().getGreeting());
+    }
+}
